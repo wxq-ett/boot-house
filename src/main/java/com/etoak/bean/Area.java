394f0243a99d2945b781ca55acad4a7f007eb21f
wxq <2692933430@qq.com>
2020-04-20 17:20:58 +0800

    地区查询接口开发完毕

---
+++ src/main/java/com/etoak/bean/Area.java
@@ -0,0 +1,13 @@
+package com.etoak.bean;
+
+import lombok.Data;
+
+@Data
+public class Area {
+
+    private Integer id;
+    private Integer pid;
+    private String name;
+    private Integer sort;
+
+}
